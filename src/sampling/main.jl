ccb673782f698fbd39d105e1d33d8d4965e7e18f
ShoSampo <shosampo@gmail.com>
2023-11-01 16:33:00 +0900

    サンプルサイズでベクトル演算化

--- src/sampling/main.jl
+++ src/sampling/main.jl
@@ -6,13 +6,9 @@ function main(args)
     s = ArgParseSettings()
 
     @add_arg_table! s begin
-        "--S"
-        # help = "Number of samples"
-        default = 10_000
-        arg_type = Int
         "--T"
         # help = "Total number of time steps"
-        default = 100_000
+        default = 10_000
         arg_type = Int
         "--p"
         default = 0.9 # 0.99
@@ -24,28 +20,33 @@ function main(args)
         # help = "Exponent parameter alpha"
         default = 1.0
         arg_type = Float64
+        "--sample"
+        # help = "Sample size"
+        default = 10_000
+        arg_type = Int
     end
 
     parsed_args = parse_args(args, s)
-    S = parsed_args["S"]
     T = parsed_args["T"]
     p = parsed_args["p"]
     q = parsed_args["q"]
     alpha = parsed_args["alpha"]
+    sample = parsed_args["sample"]
 
     # Log the simulation parameters
     println("Running simulation with the following parameters:")
-    println("S = $(int_to_SI_prefix(S)), T = $(int_to_SI_prefix(T)), p = $(p), q = $(q), alpha = $(alpha)")
+    println("T = $(int_to_SI_prefix(T)), p = $(p), q = $(q), alpha = $(alpha), sample = $(int_to_SI_prefix(sample))")
 
     # Run the simulation
-    Ct = Simulation.simulate_samples(S, T, p, q, alpha)
+    # Ct = Simulation.simulate_samples(S, T, p, q, alpha)
+    Ct = Simulation.simulate(T, p, q, sample)
 
     # Output Z values to CSV
     dir = "data/Ct"
     if !isdir(dir)
         mkpath(dir)
     end
-    filename = joinpath(dir, "S$(int_to_SI_prefix(S))_T$(int_to_SI_prefix(T))_p$(p)_q$(q)_alpha$(alpha).csv")
+    filename = joinpath(dir, "T$(int_to_SI_prefix(T))_p$(p)_q$(q)_alpha$(alpha)_sample$(int_to_SI_prefix(sample)).csv")
     save_to_csv(Ct, filename)
     end
 
--- src/sampling/simulation.jl
+++ src/sampling/simulation.jl
@@ -9,44 +9,38 @@ function decision_function(z::Float64, p::Float64, q::Float64)::Float64
     return (1 - p) * q + p * z
 end
 
-function simulate(T::Int, p::Float64, q::Float64, progressBar::Progress)::Matrix{Float64}
-    Sum_of_zr_t_X1 = zeros(Float64, T, 2)
+function simulate(T::Int, p::Float64, q::Float64, Sample::Int)::Vector{Float64}
+    # Array to store Z values for each initial value of x1
+    Z_by_x1 = zeros(Float64, T, Sample, 2)
 
-    X = zeros(Int, T)
-    S = zeros(Float64, T)
-    Z = zeros(Float64, T)
+    progressBar = Progress(T * 2, 1)
+
     for x1 in 0:1
         next!(progressBar)
-        X[1] = x1
-        S[1] = X[1]
-        Z[1] = S[1]
+        # Initialize arrays for computation
+        X = zeros(Int, T, Sample)
+        S = zeros(Float64, T, Sample)
+        Z = zeros(Float64, T, Sample)
+
+        # Initial values
+        X[1, :] .= x1
+        S[1, :] .= x1
+        Z[1, :] .= x1
+
         for t in 2:T
-            X[t] = rand(Float64) < decision_function(Z[t-1], p, q)
-            S[t] = S[t-1] + X[t]
-            Z[t] = S[t] / t
+            next!(progressBar)
+            decisions = decision_function.(Z[t-1, :], p, q)
+            rand_vals = rand.(Float64, Sample)
+            X[t, :] .= rand_vals .< decisions
+            S[t, :] = S[t-1, :] .+ X[t, :]
+            Z[t, :] = S[t, :] ./ t
         end
-        Sum_of_zr_t_X1[:, x1+1] += Z
-    end
-    return Sum_of_zr_t_X1
-end
 
-function simulate_samples(Sample::Int, T::Int, p::Float64, q::Float64, alpha::Float64)::Vector{Float64}
-    # Initialize a SharedArray for storing results
-    Sum_of_zr_t_X1_samples = SharedArray{Float64}(T, 2, Sample)
-
-    progressBar = Progress(Sample * 2, 1, "Samples: ")
-    ProgressMeter.update!(progressBar, 0)
-
-    # Parallelize the simulation across the specified number of samples
-    @sync @distributed for i in 1:Sample
-        Sum_of_zr_t_X1_samples[:, :, i] = simulate(T, p, q, progressBar)
+        Z_by_x1[:, :, x1+1] = Z
     end
 
-    println("Finished sampling")
-
-    # Calculate mean and standard deviation values for Ct values
-    total_Sum_of_zr_t_X1 = sum(Sum_of_zr_t_X1_samples, dims=3)[:,:,1]
-    Ct = (total_Sum_of_zr_t_X1[:, 2] .- total_Sum_of_zr_t_X1[:, 1]) ./ Sample
+    # Calculate Ct
+    Ct = (mean(Z_by_x1[:, :, 2], dims=2) .- mean(Z_by_x1[:, :, 1], dims=2))[:]
 
     return Ct
 end
